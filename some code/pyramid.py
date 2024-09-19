bc64b27c3248362844cebeec471ed0b592af7501
Alexander Bedine <ajb20dd@fsu.edu>
2024-09-19 09:48:36 -0400

    idk

---
+++ some code/pyramid.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+def pyramid(n)
+    for i in range(1, n + 1)
+        str_brick = '*'
+        str_space = ' '
+        print((str_space * (n - i)) + (str_brick * (2 * i - 1)) + (str_space * (n - i)))
+
+pyramid(n)
