19a90bd4379b234868211dcab9844b9cf03019c5
Alexander Bedine <ajb20dd@fsu.edu>
2024-10-03 09:36:13 -0400

    Update pyramid.py
    fixed formatting which caused invalid code

--- some code/pyramid.py
+++ some code/pyramid.py
@@ -1,7 +1,7 @@
 n = int(input())
 
-def pyramid(n)
-    for i in range(1, n + 1)
+def pyramid(n):
+    for i in range(1, n + 1):
         str_brick = '*'
         str_space = ' '
         print((str_space * (n - i)) + (str_brick * (2 * i - 1)) + (str_space * (n - i)))
